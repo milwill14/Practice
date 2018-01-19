e703ab391ede1fc92690e82ac7732ec37107cea6
milwill14 <35510343+milwill14@users.noreply.github.com>
2018-01-18 19:02:42 -0500

    Add files via upload

--- First.java
+++ First.java
@@ -1,9 +1,19 @@
+// Milen W.
+// My project that integrates all the skills
+// I've learned 
 
 public class First {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("hello world");
+		
+
 
 	}
 
+}
+
+class Car {
+	private int speed;
+	
 }
