fb411c809f491376252b83717c1710289a967c2a
milwill14 <35510343+milwill14@users.noreply.github.com>
2018-02-11 00:02:30 -0500

    Add files via upload

---
+++ Innegrate1.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+// Created by Milen Williams
+// This program informs users of their grades
+
+public class Innegrate1 {
+
+	private static Scanner input;
+
+	public static void main(String[] args) {
+		
+		String greet = "Hello user!";
+		
+		System.out.println(greet);
+		
+		input = new Scanner(System.in); 
+		
+		System.out.println("Enter your name: ");
+		String line = input.nextLine();
+		
+		// This tells them what they entered
+		System.out.println("Hello " + line);
+		
+		System.out.println("Type Start to Begin");
+		String text = input.nextLine();
+		
+		switch (text) {
+		case "start":
+			System.out.println("Proceed with program");
+			break;
+			
+		case "Start":
+			System.out.println("Proceed with program");
+		default:
+			System.out.println("Error");
+		}
+		
+		System.out.println("Please enter your score");
+		double grade = input.nextDouble();
+		
+		if(grade < 60) {
+			System.out.println("You failed.");
+		}
+		else {
+			System.out.println("Congratulations! You passed.");
+			}
+		}
+}
